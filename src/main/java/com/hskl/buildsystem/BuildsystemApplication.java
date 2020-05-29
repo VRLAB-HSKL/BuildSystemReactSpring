45c50422e921951768a3cd831e3378508dfb172f
Marco H <mahe0046@stud.hs-kl.de>
2020-05-29 16:44:51 +0200

    initial project setup

---
+++ src/main/java/com/hskl/buildsystem/BuildsystemApplication.java
@@ -0,0 +1,13 @@
+package com.hskl.buildsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hskl/buildsystem/BuildsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hskl.buildsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
