f19d8801ca361bb0de9cfb9ec38eaff79031847f
Marco H <mahe0046@stud.hs-kl.de>
2020-06-20 05:24:45 +0200

    add comments to @UnityService

--- src/main/java/com/hskl/buildsystem/service/UnityService.java
+++ src/main/java/com/hskl/buildsystem/service/UnityService.java
@@ -9,11 +9,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * This class provides the rest of the interfaces for communication with unity
+ */
 @RestController
 public class UnityService {
 
+    //the logger
     private final Logger log = LoggerFactory.getLogger(UnityService.class);
 
+    /**
+     * receive unity project information to store
+     * @param data : unity project data as json
+     */
     @PostMapping("/api/unity/data")
     public void createProjectData(@RequestBody UnityData data) {
       log.info("Request from Unity: {}", data);
