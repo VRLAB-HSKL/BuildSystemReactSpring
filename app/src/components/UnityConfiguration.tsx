b018126962331ab804ababf18fb35d7e85f4c4d9
Marco H <mahe0046@stud.hs-kl.de>
2020-07-06 11:39:23 +0200

    add UnityConfiguration tsx

---
+++ app/src/components/UnityConfiguration.tsx
@@ -0,0 +1,30 @@
+import React, {Component} from 'react'
+
+interface IProps {
+
+}
+
+interface IState {
+
+}
+
+
+class UnityConfiguration extends Component<IState, IProps> {
+
+    constructor(props: IProps) {
+        super(props);
+
+        this.state = {
+
+        };
+    }
+
+    render() {
+
+        return(
+            <div>Test</div>
+        );
+    }
+}
+
+export default UnityConfiguration;
